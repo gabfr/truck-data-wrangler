cbeed51f44098aa4301743e284c71614cbd6c50e
Gabriel <eu@gabrielf.com>
2019-08-06 15:09:39 -0300

    Fixing the timescaledb table to follow the raw files schema

--- common/sql_queries.py
+++ common/sql_queries.py
@@ -4,7 +4,7 @@ drop_jerked_truck_events_table = "DROP TABLE IF EXISTS jerked_truck_events"
 create_jerked_truck_events_table = """
     CREATE TABLE IF NOT EXISTS jerked_truck_events (
         date_timestamp TIMESTAMP NOT NULL,
-        event_type VARCHAR(255) NOT NULL,
+        -- event_type VARCHAR(255) NOT NULL,
         label VARCHAR(50) NOT NULL,
         accel_x DECIMAL(20, 17),
         accel_y DECIMAL(20, 17),
@@ -24,7 +24,8 @@ create_jerked_truck_events_table = """
         is_breaking INTEGER,
         is_turning_left INTEGER,
         is_turning_right INTEGER,
-        PRIMARY KEY (date_timestamp, event_type, label)
+        -- PRIMARY KEY (date_timestamp, event_type, label)
+        PRIMARY KEY (date_timestamp, label)
     )
 """
 
