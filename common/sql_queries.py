2c1743b510d20ec4eea9cba6f82a6460e348b403
Gabriel <eu@gabrielf.com>
2019-08-06 12:33:56 -0300

    Adding the SQL queries to give us the answer for the batch part of the challenge

--- common/sql_queries.py
+++ common/sql_queries.py
@@ -28,3 +28,61 @@ create_jerked_truck_events_table = """
     )
 """
 
+
+average_acceleration_query = """
+    SELECT 
+        event_type, 
+        label, 
+        AVG(accel_x) AS avg_accel_x,
+        AVG(accel_y) AS avg_accel_y,
+        AVG(accel_z) AS avg_accel_z
+    FROM 
+        truck_events 
+    GROUP BY
+        event_type, label
+    ORDER BY
+        event_type, label
+"""
+
+
+max_jerk_x_query = """
+    SELECT 
+        event_type, 
+        label, 
+        from_unixtime(cast(((timestamp / 1000) / 1000) as bigint),'yyyy-MM-dd HH:mm:ss.SSS') AS timestamp,
+        jerk_x
+    FROM 
+        jerked_truck_events 
+    ORDER BY
+        jerk_x DESC
+    LIMIT 1
+"""
+
+
+max_jerk_y_query = """
+    SELECT 
+        event_type, 
+        label, 
+        from_unixtime(cast(((timestamp / 1000) / 1000) as bigint),'yyyy-MM-dd HH:mm:ss.SSS') AS timestamp,
+        jerk_y
+    FROM 
+        jerked_truck_events 
+    ORDER BY
+        jerk_y DESC, event_type, label
+    LIMIT 1
+"""
+
+
+max_jerk_z_query = """
+    SELECT 
+        event_type, 
+        label, 
+        from_unixtime(cast(((timestamp / 1000) / 1000) as bigint),'yyyy-MM-dd HH:mm:ss.SSS') AS timestamp,
+        jerk_z
+    FROM 
+        jerked_truck_events 
+    ORDER BY
+        jerk_z DESC, event_type, label
+    LIMIT 1
+"""
+
