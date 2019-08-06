b48405342c83a2f2f07d8cd47f233d60def72430
Gabriel <eu@gabrielf.com>
2019-08-06 20:50:11 -0300

    Making a version of the udf accessible through python

--- common/udfs.py
+++ common/udfs.py
@@ -2,8 +2,12 @@ from pyspark.sql.functions import udf
 from pyspark.sql.types import StringType
 
 
-@udf(StringType())
-def get_underscore_prefix(s):
+def _get_underscore_prefix(s):
     if '://' in s:
         s = s.split('/')[x-1]
     return "_".join(s.split("_")[:-1])
+
+
+@udf(StringType())
+def get_underscore_prefix(s):
+    return _get_underscore_prefix(s)
