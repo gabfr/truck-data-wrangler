f93d50446705642d6de626869d33a3c643858fd0
Gabriel <eu@gabrielf.com>
2019-08-06 12:34:27 -0300

    The app batch :)

---
+++ app_batch.py
@@ -0,0 +1,21 @@
+from common.connections import create_spark_session
+from common.sql_queries import average_acceleration_query, max_jerk_x_query, max_jerk_y_query, max_jerk_z_query
+from batch.etl import unify_separate_event_files, calculate_jerk_from_truck_events
+
+
+def main():
+    spark = create_spark_session()
+    
+    truck_events_df = unify_separate_event_files(spark)
+
+    spark.sql(average_acceleration_query).show(truncate=False)
+    
+    jerked_truck_events_df = calculate_jerk_from_truck_events(spark)
+    
+    spark.sql(max_jerk_x_query).show(truncate=False)
+    spark.sql(max_jerk_y_query).show(truncate=False)
+    spark.sql(max_jerk_z_query).show(truncate=False)
+
+
+if __name__ == "__main__":
+    main()
